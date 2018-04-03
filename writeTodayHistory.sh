72e7eaafdf913fa0bed11a89c634775cdfe702b8
Javier Rodriguez Vivas <jarodrivi@gmail.com>
2018-04-03 22:51:03 +0200

    EN PROCESO

--- writeTodayHistory.sh
+++ writeTodayHistory.sh
@@ -1,2 +1,10 @@
 #Este script copia el historial de los comandos que hacemos en el terminal desde un punto. 
 #Antes de empezar la clase es imprescindible poner un comando echo con algo característico para que el script lo busque.
+
+START= grep Empecemos -n ~/.history | grep "^[0-9]*" -o | tail -n -1
+
+echo $START
+
+cat ~/.history | tail -n +$START
+
+##EN PROCESO
