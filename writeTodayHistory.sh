85f703492f78922c5a4d21044811daf57f5bef4f
Javier Rodriguez Vivas <jarodrivi@gmail.com>
2018-04-03 19:47:58 +0200

    Script de historial

---
+++ writeTodayHistory.sh
@@ -0,0 +1,2 @@
+#Este script copia el historial de los comandos que hacemos en el terminal desde un punto. 
+#Antes de empezar la clase es imprescindible poner un comando echo con algo característico para que el script lo busque.
