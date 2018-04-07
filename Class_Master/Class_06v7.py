390d3dd0759692a9abfbada94276afefd547f2d1
Javier Rodriguez Vivas <jarodrivi@gmail.com>
2018-04-07 11:03:29 +0200

    Clase 06v7 (2)

--- Class_Master/Class_06v7.py
+++ Class_Master/Class_06v7.py
@@ -1,2 +1,34 @@
 print('Clase Master 06v7')
 
+def by2(x):
+	return 2*x
+
+def lby2(x):
+	by2_b= lambda y: y*2
+	return by2_b(x)
+
+def callme(f,x):
+	return f(x)
+
+def ex1(l):
+	square= lambda x: x*x
+	return list(map(square,l))
+
+def isVocal(s):
+	vocals="aeiou"
+	k=''
+	for i in s:
+		if i in vocals:
+			k+=i.upper()
+		else:
+			k+=i.lower()
+	return k
+
+def ex2(l):
+	return list(map(isVocal,l))
+
+def isVocals_2(s):
+	return ''.join(list(map(lambda x: x.upper() if x in "aeiouAEIOU" else x.lower(),s)))
+
+def ex2_2(l):
+	return list(map(isVocals_2,l))
