c06517f278ca0877a02aa68b378f20b6b880a928
Javier Rodriguez Vivas <jarodrivi@gmail.com>
2018-04-07 12:41:11 +0200

    End Basic Python

--- Class_Master/Class_06v7.py
+++ Class_Master/Class_06v7.py
@@ -10,7 +10,7 @@ def lby2(x):
 def callme(f,x):
 	return f(x)
 
-def ex1(l):
+def ex3_1(l):
 	square= lambda x: x*x
 	return list(map(square,l))
 
@@ -24,11 +24,18 @@ def isVocal(s):
 			k+=i.lower()
 	return k
 
-def ex2(l):
+def ex3_2(l):
 	return list(map(isVocal,l))
 
 def isVocals_2(s):
 	return ''.join(list(map(lambda x: x.upper() if x in "aeiouAEIOU" else x.lower(),s)))
 
-def ex2_2(l):
+def ex3_2_2(l):
 	return list(map(isVocals_2,l))
+
+def ex3_3(l,c):
+	return list(filter(lambda x: not(c in x),l,c))
+
+def ex5_2(l):
+	return sorted(l,key=lambda x:len(set(x.lower())),reverse=True)
+
